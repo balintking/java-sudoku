f04365df45aa47db81c286f81b11ffd84d5148aa
Bálint Király <balintkiraly.dev@gmail.com>
2023-11-30 04:33:55 +0100

    Implement Cell

--- sudoku/Cell.java
+++ sudoku/Cell.java
@@ -2,8 +2,46 @@ package sudoku;
 
 import javax.swing.*;
 
+/**
+ * Represents a single cell on the GameBoard
+ */
 public class Cell extends JTextField {
-    /*
-    represents a cell in the game
+
+    /**
+     * The value stored in the cell
      */
+    private int value;
+
+    /**
+     * Tells if the value of the cell is given, and so that it can not be edited
+     */
+    private boolean isGiven;
+
+
+    /**
+     * Constructor for Cell
+     * @param value The value stored in the cell
+     * @param isGiven Tells if the value of the cell is given, and so that it can not be edited
+     */
+    public Cell(int value, boolean isGiven) {
+        this.value = value;
+        this.isGiven = isGiven;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isGiven() {
+        return isGiven;
+    }
+
+    public void setGiven(boolean given) {
+        isGiven = given;
+    }
 }
